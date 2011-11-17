fd0511c2fba52858396fa8238ee96bc24d2561fd
Jiawei Ou <ouj@cs.dartmouth.edu>
2011-11-17 16:02:10 -0500

    application and icon

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#ifdef WIN32
+#include <gl/glew.h>
+#include <gl/glut.h>
+#else
+#include <OpenGL/OpenGL.h>
+#include <GLUT/GLUT.h>
+#endif // WIN32
+
+
+int main(int argc, char** argv) {
+    return 0;
+}
