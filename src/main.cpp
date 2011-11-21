dc848d4afe53baeee9bdd5b9fa277100e2c9dbda
Jiawei Ou <ouj@cs.dartmouth.edu>
2011-11-20 19:29:47 -0500

    debug code

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,22 @@
 #endif // WIN32
 #include <XnCppWrapper.h>
 
+#include "debug.h"
+
 xn::Context context;
 
 int main(int argc, char** argv) {
-    printf("finish");
+    XnStatus ret = context.Init();
+    if(!error_if_not(ret == XN_STATUS_OK, "failed to init openni context"))
+        return -1;
+    
+    xn::DepthGenerator depth;
+    ret = depth.Create(context);
+    if(!error_if_not(ret == XN_STATUS_OK, "failed to create depth node"))
+        return -1;
+    
+    
+    
+    context.Shutdown();
     return 0;
 }
